5d0396cc9c4aa2931373f24e315aa6363349f5a9
sheelajyothsna <sheela.jyothsna504@gmail.com>
2017-08-24 10:49:54 +0530

    newbags

--- CSPP1_2017_part-1_20176077-bagOfCodes.py
+++ CSPP1_2017_part-1_20176077-bagOfCodes.py
@@ -1,6 +1,7 @@
 import os                                                           
 import math
 import string
+import re
 class plagiarism():
     def __init__(self):
         self
@@ -10,16 +11,18 @@ class plagiarism():
         p=str(f.read())
         p=p.lower()
         p=p.replace("\n"," ").replace("\t"," ").replace("."," ").replace(","," ")
+        p=re.sub('[@#$%^&*(){}?|/\:"-+=!~`;]','', p)
         p=p.split(" ")
-        p=[i.strip(string.punctuation)for i in p]
+        #p=[i.strip(string.punctuation)for i in p]
         
         for k in p:
-                if k not in d:                                   # counting no of values and storing in dictionary """
-                    d[k]=1
-                else:
+                if k in d:                                   # counting no of values and storing in dictionary """
                     d[k]+=1
+                else:
+                    d[k]=1
         if '' in d:
             del d['']
+        #print(d)
         f.close()
         return d
     def dot_num(self,d1,d2):
@@ -27,45 +30,44 @@ class plagiarism():
         for k in d1:
             for o in d2:                                        # dot product of frequencies """
                 if k==o:
-                    u=(d1[k]*d2[o])
-                    w=w+u
+                    w+=(d1[k]*d2[o])
+                    
         return w
-    def eucild(self,x,y):
+    def eucild(self,x):                                                       # eucildian product"""
         p1=0
-        p2=0                                                       # eucildian product"""
         for k in x:
             p1+=(x[k])**2
-        for k in y:
-            p2+=(y[k])**2
             
         p1=math.sqrt(p1)
-        p2=math.sqrt(p2)
-        return p1*p2
+        return p1
          
     def res(self,w,v):
-        L1=(round((w/v)*100))                            #distance calculation """
+        #print(w)
+        #print(v)
+        L1=(((w/v)*100))                            #distance calculation """
         if L1==0:
             return 1.57
-        return L1 
+        return round(L1,2) 
     def intake(self):
         n=input('enter directory')
         z=os.listdir(n)
         os.chdir(n)
-        t=[]
         L1=[]
         for k in range(len(z)):
             for o in range(len(z)):
                 if k==o:                                           # taking multiple files  and calling functions """              """printing outputs"""
                      L1.append("0")
                 else:
-                    t.append(c.dict(z[k]))
-                    t.append(c.dict(z[o]))
-                    g1=c.dot_num(t[0],t[1])
-                    e=c.eucild(t[0],t[1])
+                    t1=(c.dict(z[k]))
+                    print(t1)
+                    t2=(c.dict(z[o]))
+                    print(t2)
+                    g1=c.dot_num(t1,t2)
+                    e1=c.eucild(t1)
+                    e2=c.eucild(t2)
+                    e=e1*e2
                     L1.append(str(c.res(g1,e))+'%')
-                
-                    t=[]
-            #print(z[k])
+            print(z[k])
             print(L1)
             L1=[]
 c=plagiarism()
